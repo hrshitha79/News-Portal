534948a85268ac6eda50a853766cd63750e0f649
hrshitha79 <96517805+hrshitha79@users.noreply.github.com>
2022-10-30 19:10:51 +0530

    initail commit

---
+++ admin/includes/leftsidebar.php
@@ -0,0 +1,72 @@
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                        	<li class="menu-title">ನ್ಯಾವಿಗೇಶನ್</li>
+
+                            <li class="has_sub">
+                                <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> ಡ್ಯಾಶ್ ಬೋರ್ಡ್ </span> </a>
+                         
+                            </li>
+
+
+
+               
+
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> ವರ್ಗ </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                	<li><a href="add-category.php">ವರ್ಗ ಸೇರಿಸು</a></li>
+                                    <li><a href="manage-categories.php">ವರ್ಗ ನಿರ್ವಹಿಸಿ</a></li>
+                                </ul>
+                            </li>
+
+    <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span>ಉಪವರ್ಗ </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-subcategory.php">ಉಪವರ್ಗ ಸೇರಿಸು </a></li>
+                                    <li><a href="manage-subcategories.php">ಉಪವರ್ಗ ವನ್ನು ನಿರ್ವಹಿಸಿ</a></li>
+                                </ul>
+                            </li>               
+  <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> ಪೋಸ್ಟ್ ಗಳು </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-post.php">ಪೋಸ್ಟ್ ಗಳನ್ನು ಸೇರಿಸು </a></li>
+                                    <li><a href="manage-posts.php">ಪೋಸ್ಟ್ ಗಳನ್ನು ನಿರ್ವಹಿಸಿ</a></li>
+                                     <li><a href="trash-posts.php">ಅಳಿಸಿದ ಪೋಸ್ಟ್ ಗಳು</a></li>
+                                </ul>
+                            </li>  
+                     
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> ಪುಟಗಳು </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="aboutus.php">ನಮ್ಮ ಬಗ್ಗೆ</a></li>
+                                    <li><a href="contactus.php">ನಮ್ಮನ್ನು ಸಂಪರ್ಕಿಸಿ</a></li>
+                                </ul>
+                            </li>
+   <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> ಪ್ರತಿಕ್ರಿಯೆಗಳು </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                  <li><a href="unapprove-comment.php">ಅನುಮೋದನೆಗಾಗಿ ಕಾಯುತ್ತಿದೆ </a></li>
+                                    <li><a href="manage-comments.php">ಅನುಮೋದಿತ ಪ್ರತಿಕ್ರಿಯೆಗಳು</a></li>
+                                </ul>
+                            </li>   
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+                    <div class="help-box">
+                        <h5 class="text-muted m-t-0">ಸಹಾಯಕ್ಕಾಗಿ ?</h5>
+                        <p class=""><span class="text-custom">ಇಮೇಲ್:</span> <br/> harshitha79@gmail.com</p>
+                    </div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
